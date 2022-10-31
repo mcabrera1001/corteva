971b65e35f7bccde68adea776992da0981ef1cbb
cabrera.manuel4569@gmail.com <cabrera.manuel4569@gmail.com>
2022-10-31 16:30:57 -0400

    started dockerizing the app

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # corteva_challenge
+
+docker compose -f docker-compose.yml up --build
--- models/weather.py
+++ models/weather.py
@@ -3,10 +3,10 @@ from config.flask_and_database import db
 class Weather(db.Model):
     active = db.Column(db.Boolean, default=True) # for soft delete functionality.
     date = db.Column(db.String(8), unique=True)
-    station = db.Column(db.String(11))
+    station = db.Column(db.String(11), primary_key=True)
     min_temperature = db.Column(db.Float, nullable=True)
     max_temperature = db.Column(db.Float, nullable=True)
     precipitation = db.Column(db.Float, nullable=True)
 
-with db.app_context():
-    db.create_all()
+
+db.create_all()
